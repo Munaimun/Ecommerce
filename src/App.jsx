68b65a783d75d6c26ec0db40d2c8741b4541cd32
munaimun <munaimunz@gmail.com>
2024-10-12 21:13:31 +0600

    created the project directory and setup the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import CategoryItem from "./components/category.item";
+
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "Womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "Mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+function App() {
+  return (
+    <div className="max-w-screen-xl flex m-auto flex-wrap text-center justify-center">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/category.item.jsx
@@ -0,0 +1,21 @@
+const CategoryItem = ({ category }) => {
+  //destructuring the data
+  const { imageUrl, title } = category;
+
+  return (
+    <div className="w-80 m-4 border border-black rounded-md p-3 shadow-xl animate__animated animate__pulse flex flex-col justify-between">
+      <img
+        src={imageUrl}
+        alt={title}
+        className="mb-4 animate__animated animate__zoomIn"
+      />
+
+      <div className="text-center">
+        <p className="text-lg font-bold">{title}</p>
+        <p className="text-sm">Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
